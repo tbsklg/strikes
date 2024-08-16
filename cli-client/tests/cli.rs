5774b64200dc51de37fbc7bdcfe0a60c30a60ec2
Tobias Klug <tbs.klg@gmail.com>
2024-08-16 15:12:56 +0200

    Add tests for ls of strikes

--- cli-client/tests/cli.rs
+++ cli-client/tests/cli.rs
@@ -27,3 +27,16 @@ fn add_strike() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn list_strikes() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("./tests/fixtures/db.json")?;
+    file.write_str("{\"guenther\": 1}")?;
+
+    let mut cmd = Command::cargo_bin("strikes")?;
+
+    cmd.arg("--db-path").arg(file.path()).arg("ls");
+    cmd.assert().success().stdout("{\"guenther\": 1}\n");
+
+    Ok(())
+}
