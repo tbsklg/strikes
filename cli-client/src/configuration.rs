06400706b840caf4c080ed10707df5f7620298ee
Tobias Klug <tbs.klg@gmail.com>
2024-08-17 13:54:51 +0200

    Test default configuration

--- cli-client/src/configuration.rs
+++ cli-client/src/configuration.rs
@@ -60,6 +60,20 @@ mod tests {
     }
 
     #[test]
+    fn parse_default_config() {
+        std::env::set_var("HOME", "/home/user");
+
+        let configuration = get_configuration(PathBuf::from("tests/fixtures/empty_config.yaml"))
+            .unwrap_or_default();
+
+
+        assert_eq!(
+            configuration.local.unwrap().db_path,
+            PathBuf::from("/home/user/.strikes/db.json")
+        );
+    }
+
+    #[test]
     #[should_panic]
     fn parse_invalid_config() {
         get_configuration(PathBuf::from("tests/fixtures/invalid_config.yaml")).unwrap();
